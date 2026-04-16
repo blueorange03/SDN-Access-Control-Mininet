7ecb8b2dce10c2ba1f96fcee82a203d67adcb934
Draj <saidheeraj291@gmail.com>
2026-04-16 18:52:45 +0530

    Final

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
 # SDN-Access-Control-Mininet
+
+
+# Student Details
+
+Name: Patnaikuni Sai Dheeraj
+SRN: PES2UG24CS341
+
+
+
+# Description
+
+This project implements access control in Software Defined Networking using the Ryu controller and Mininet.
+
+The controller filters traffic based on MAC addresses. Only selected hosts are allowed to communicate, while others are blocked.
+
+
+
+# Objective
+
+* To understand SDN architecture
+* To implement access control using a controller
+* To simulate a network using Mininet
+
+
+
+# Topology
+
+The topology consists of:
+
+* 1 switch (s1)
+* 3 hosts (h1, h2, h3)
+
+All hosts are connected to a single switch.
+
+MAC addresses:
+
+* h1 → 00:00:00:00:00:01
+* h2 → 00:00:00:00:00:02
+* h3 → 00:00:00:00:00:03
+
+
+
+## Access Control Logic
+
+Whitelist:
+
+* h1
+* h2
+
+Blocked:
+
+* h3
+
+Behavior:
+
+* h1 can communicate with h2
+* h2 can communicate with h1
+* h3 is blocked from communicating with other hosts
+
+
+
+# How to Run
+
+Activate environment:
+
+```bash
+source ryu-env/bin/activate
+```
+
+Run controller:
+
+```bash
+ryu-manager access_control.py
+```
+
+Run Mininet:
+
+```bash
+sudo mn --topo single,3 --mac --controller=remote,ip=127.0.0.1,port=6633
+```
+
+
+
+# Results
+
+Allowed:
+
+```bash
+h1 ping h2
+h2 ping h1
+```
+
+Blocked:
+
+```bash
+h3 ping h1
+h3 ping h2
+```
+
+The controller logs show blocked MAC address:
+BLOCKED: 00:00:00:00:00:03
+
+
+
+# Files
+
+* access_control.py
+* README.md
+* screenshots folder
+
+
+
---
+++ access_control.py
@@ -0,0 +1,90 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
+
+class ACLSwitch(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    WHITELIST = {
+        "00:00:00:00:00:01",
+        "00:00:00:00:00:02"
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(ACLSwitch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        src = eth.src
+        dst = eth.dst
+        dpid = datapath.id
+        in_port = msg.match['in_port']
+
+        # initialize switch table
+        self.mac_to_port.setdefault(dpid, {})
+
+        # learning
+        self.mac_to_port[dpid][src] = in_port
+
+        # ACL check
+        if src not in self.WHITELIST:
+            self.logger.info(f"BLOCKED: {src}")
+            return
+
+        # forwarding logic
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data
+        )
+        datapath.send_msg(out)
